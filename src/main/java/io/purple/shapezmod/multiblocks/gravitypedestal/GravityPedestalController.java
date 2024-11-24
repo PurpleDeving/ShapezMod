caab94952f6c1502cc2e57090e7147ef76d58846
PurpleDeving <lazzydeving@gmail.com>
2024-11-24 21:57:17 +0100

    Formulated TODO's for the main System and the Gravity Pedestal

---
+++ src/main/java/io/purple/shapezmod/multiblocks/gravitypedestal/GravityPedestalController.java
@@ -0,0 +1,24 @@
+package io.purple.shapezmod.multiblocks.gravitypedestal;
+
+import io.purple.purplelib.multiblock.ControllerBlock;
+
+public class GravityPedestalController extends ControllerBlock {
+
+
+    public GravityPedestalController(Properties properties) {
+        super(properties);
+    }
+
+    // TODO 1. Create the block
+    // TODO 2. Look at X3R for Rendering or look at POWAH for animation or look for something that builds up with each spike of the altar above
+    // TODO 3. Right click when not assembled should give message (maybe ControllerBlock function)
+    // TODO 4. Look at pedestal mod for how to float thingy
+    // TODO 5. Change thingy to corresponding black hole. How can it grow? What happens if there are blocks there ?
+    // TODO 6. Look at vacuum hopper for how to "eat" items
+    // TODO 7. Internal "recipe" handler ? What even is a recipe for this? How to store in save what progress was made? How often do you store that?
+    // TODO 8. How to even send items in ? If it just absorbs then it needs items to be launched, or all people will just use a lame dropper from above?
+    // TODO 9. Alternatively some guided floating ?
+    // TODO 10. Differentiation from this MB to all of the mod ? IDK ? > Will this be the absolute main thing, or just 1 thing ?
+    // TODO 11. Interaction of the system ? Possible other MB with Base + Terminal
+    // TODO 12. Interaction of the system with FTB Quests or Gamestages etc.?
+}
